199428394df2037a0be349a375b9e312fadfcb0e
Hayden Roney <roneyhayden@gmail.com>
2023-12-02 23:52:45 -0600

    Lab finished

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    function clicked() {
+        console.log("Teehee!");
+    }
+
+    function focused() {
+        console.log("Good!")
+    }
+
+    function blurred() {
+        console.log("Hey! Eyes on me!")
+    }
+
+    return <button onFocus={focused} onBlur={blurred} onClick={clicked} >Eyes on me!</button>;
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,20 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+    function handleChange(event) {
+        console.log(`Entering password...`);
+    }
+
     return (
-        <div></div>
-    )
+        <div>
+            <input
+                type="password"
+                name="search"
+                onChange={handleChange}
+                placeholder="Enter password"
+            />
+        </div>
+    );
 }
 
 export default Keypad;
